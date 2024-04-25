bd458ce751ee7683f7b6a95371d09ceb84d1a95c
Keith D Commiskey <atlassian@kdcinfo.com>
2024-04-25 05:08:29 -0700

    Added a `helpers` folder with abstract `PumpApp`.

---
+++ tictactoe/test/helpers/pump_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class PumpApp {
+  static Widget materialApp(Widget child) => MaterialApp(
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+          colorSchemeSeed: const Color(0xFF800000),
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          colorSchemeSeed: const Color(0xFF800000),
+        ),
+        home: child,
+      );
+}
