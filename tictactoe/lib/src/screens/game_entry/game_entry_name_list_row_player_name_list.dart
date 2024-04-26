ee2eddf499dd2723c3c44930a73b96aa427eabf0
Keith D Commiskey <atlassian@kdcinfo.com>
2024-04-26 07:12:58 -0700

    Added mock `DropdownMenu` to `PlayerNameList`.

--- tictactoe/lib/src/screens/game_entry/game_entry_name_list_row_player_name_list.dart
+++ tictactoe/lib/src/screens/game_entry/game_entry_name_list_row_player_name_list.dart
@@ -5,8 +5,26 @@ class GameEntryNameListRowPlayerNameList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const DropdownMenu(
-      dropdownMenuEntries: [],
+    return const DropdownMenu<int>(
+      // Hintext should be a label less than 10 characters
+      hintText: 'Select',
+      width: 125,
+      dropdownMenuEntries: [
+        /// 'Select from previous names:'
+        DropdownMenuEntry(
+          value: 0,
+          label: 'Previously Played Names',
+          enabled: false,
+        ),
+        DropdownMenuEntry(
+          value: 1,
+          label: 'JohnDoe',
+        ),
+        DropdownMenuEntry(
+          value: 2,
+          label: 'JaneSmith',
+        ),
+      ],
     );
   }
 }
