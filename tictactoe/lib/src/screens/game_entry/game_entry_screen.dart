891d6b6bce275a380996075fb5cd4f040925e3bf
Keith D Commiskey <atlassian@kdcinfo.com>
2024-05-01 23:40:07 -0700

    Fixed layout and tests for `GameEntryScreen`.

--- tictactoe/lib/src/screens/game_entry/game_entry_screen.dart
+++ tictactoe/lib/src/screens/game_entry/game_entry_screen.dart
@@ -9,70 +9,114 @@ class GameEntryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
-          ///
-          /// Determine available height minus static elements.
-          final availableHeight = constraints.maxHeight - 60 - 40 - 10;
+        body: LayoutBuilder(builder: (
+          BuildContext context,
+          BoxConstraints maxConstraints,
+        ) {
+          final maxWidth = maxConstraints.maxWidth;
+          final maxHeight = maxConstraints.maxHeight;
 
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 30),
-            child: availableHeight < 300
-                // For landscape or small screens, we'll move the
-                // board size and buttons to the right of the player list.
-                ? const Column(
-                    children: [
-                      Expanded(
-                        child: ColoredBox(
-                          color: Colors.lightBlue,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              GameTitleRow(),
-                              SizedBox(height: 10),
-                              Expanded(
-                                child: Row(
-                                  children: [
-                                    Expanded(child: GameEntryNameList()),
-                                    SizedBox(width: 20),
-                                    SizedBox(
-                                      width: 300,
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.start,
-                                        children: [
-                                          GameEntryBoardSizeRow(),
-                                          SizedBox(height: 20),
-                                          GameEntryButtonsRow(),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                : const SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        GameTitleRow(),
-                        SizedBox(height: 10),
-                        GameEntryNameList(),
-                        SizedBox(height: 20),
-                        GameEntryBoardSizeRow(),
-                        SizedBox(height: 20),
-                        GameEntryButtonsRow(),
-                      ],
-                    ),
-                  ),
+          return ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: maxWidth,
+              maxHeight: maxHeight,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 30),
+              child: LayoutBuilder(builder: (context, checkConstraints) {
+                /// Determine available height minus static elements.
+                final availableHeight = checkConstraints.maxHeight - 60 - 40 - 10;
+
+                return availableHeight < 300 // + 110 == 410
+                    // For landscape or small screens, we'll move the
+                    // board size and buttons to the right of the player list.
+                    ? const LayoutSmall()
+                    : const LayoutLarge();
+              }),
+            ),
           );
         }),
       ),
     );
+  }
+}
+
+class LayoutLarge extends StatelessWidget {
+  const LayoutLarge({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          GameTitleRow(),
+          SizedBox(height: 10),
+          GameEntryNameList(),
+          SizedBox(height: 20),
+          GameEntryBoardSizeRow(),
+          SizedBox(height: 20),
+          GameEntryButtonsRow(),
+        ],
+      ),
+    );
+  }
+}
+
+class LayoutSmall extends StatelessWidget {
+  const LayoutSmall({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: constraints.maxHeight,
+          maxWidth: constraints.maxWidth,
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GameTitleRow(),
+            SizedBox(height: 10),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: GameEntryNameList(),
+                        ),
+                        SizedBox(width: 20),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              GameEntryBoardSizeRow(),
+                              SizedBox(height: 20),
+                              Expanded(child: GameEntryButtonsRow()),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
--- tictactoe/test/screens/game_entry/game_entry_screen_test.dart
+++ tictactoe/test/screens/game_entry/game_entry_screen_test.dart
@@ -20,10 +20,43 @@ void main() {
         wrappedWidget = PumpApp.materialApp(widgetToTest);
       });
 
-      testWidgets('[GameEntry Screen] renders.', (WidgetTester tester) async {
-        await tester.pumpWidget(wrappedWidget);
-        final widgetFinderScreen = find.byType(GameEntryScreen);
-        expect(widgetFinderScreen, findsOneWidget);
+      group('rendering', () {
+        testWidgets('[GameEntry Screen] renders by default.', (WidgetTester tester) async {
+          await tester.pumpWidget(wrappedWidget);
+          final widgetFinderScreen = find.byType(GameEntryScreen);
+          expect(widgetFinderScreen, findsOneWidget);
+        });
+
+        // Testing for Portrait and Landscape.
+        //
+        // Minimal passing sizes for DPR 1 vs. DPR 3.
+        // tester.view.physicalSize = const Size(432, 116); // 0 over  // DPR 1
+        // tester.view.physicalSize = const Size(1294, 348); // 0 over // DPR 3
+        // tester.view.physicalSize = const Size(800, 600); // Without DPR: 266.66 200
+        //
+        testWidgets('[GameEntry Screen] renders portrait.', (WidgetTester tester) async {
+          tester.view.devicePixelRatio = 1.0; // Must be set with `physicalSize`.
+          tester.view.physicalSize = const Size(299, 599);
+
+          await tester.pumpWidget(wrappedWidget);
+          final widgetFinderScreen = find.byType(GameEntryScreen);
+          expect(widgetFinderScreen, findsOneWidget);
+
+          addTearDown(tester.view.resetDevicePixelRatio);
+          addTearDown(tester.view.resetPhysicalSize);
+        });
+
+        testWidgets('[GameEntry Screen] renders landscape.', (WidgetTester tester) async {
+          tester.view.devicePixelRatio = 1.0;
+          tester.view.physicalSize = const Size(599, 299);
+
+          await tester.pumpWidget(wrappedWidget);
+          final widgetFinderScreen = find.byType(GameEntryScreen);
+          expect(widgetFinderScreen, findsOneWidget);
+
+          addTearDown(tester.view.resetDevicePixelRatio);
+          addTearDown(tester.view.resetPhysicalSize);
+        });
       });
 
       /// Note: Deeper individual widget tests are in their own respective test files.
