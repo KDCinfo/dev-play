978e2bc68e229328efc3951fbac43a675dc61d27
Keith D Commiskey <atlassian@kdcinfo.com>
2024-05-04 02:05:58 -0700

    Widgets and tests: `GameEntryNameListRow`

--- tictactoe/lib/src/screens/game_entry/game_entry_name_list_row.dart
+++ tictactoe/lib/src/screens/game_entry/game_entry_name_list_row.dart
@@ -1,4 +1,5 @@
 import 'package:dev_play_tictactoe/src/src.dart';
+
 import 'package:flutter/material.dart';
 
 class GameEntryNameListRow extends StatelessWidget {
@@ -11,31 +12,28 @@ class GameEntryNameListRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// 'Player $playerNum Name:'
-    final playerLabel = AppConstants.playerLabel(playerNum);
+    final player = GamePlayer(playerNum: playerNum);
 
-    final screenWidth = MediaQuery.sizeOf(context).width;
+    final inputName = GameEntryNameListRowInputName(player: player);
+    const savedNames = GameEntryNameListRowPlayerNameList();
 
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-        maxHeight: 90,
-        maxWidth: screenWidth * 0.9,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 5),
-            child: Text(playerLabel),
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Wrap(
+        direction: Axis.horizontal,
+        // Adds spacing horizontally between wrapped children.
+        spacing: 10,
+        // Adds spacing vertically between wrapped lines.
+        runSpacing: 4,
+        children: <Widget>[
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 150),
+            child: inputName,
           ),
-          const SizedBox(height: 5),
-          const Row(
-            children: [
-              Expanded(
-                child: GameEntryNameListRowInputName(),
-              ),
-              GameEntryNameListRowPlayerNameList(),
-            ],
+          ConstrainedBox(
+            // Set a maxWidth to match the TextField.
+            constraints: const BoxConstraints(maxWidth: 150),
+            child: savedNames,
           ),
         ],
       ),
---
+++ tictactoe/test/screens/game_entry/game_entry_name_list_row_test.dart
@@ -0,0 +1,47 @@
+import 'package:dev_play_tictactoe/src/screens/game_entry/game_entry.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../helpers/helpers.dart';
+
+void main() {
+  group('GameEntryNameListRow Testing:', () {
+    late Widget widgetToTest;
+    late Widget wrappedWidget;
+
+    ///
+    /// [ GameEntryNameListRow Widget ]
+    ///
+
+    group('GameEntryNameListRow Widget', () {
+      setUp(() async {
+        widgetToTest = const GameEntryNameListRow(playerNum: 1);
+        wrappedWidget = PumpApp.materialApp(widgetToTest);
+      });
+
+      testWidgets('[GameEntryNameListRow Widget] renders.', (WidgetTester tester) async {
+        await tester.pumpWidget(wrappedWidget);
+        final widgetFinderBoard = find.byType(GameEntryNameListRow);
+        expect(widgetFinderBoard, findsOneWidget);
+      });
+
+      /// Note: Deeper individual widget tests are in their own respective test files.
+
+      testWidgets('[GameEntryNameListRow Widget] has a GameEntryNameListRowInputName.', (
+        WidgetTester tester,
+      ) async {
+        await tester.pumpWidget(wrappedWidget);
+        final widgetFinderTitleRow = find.byType(GameEntryNameListRowInputName);
+        expect(widgetFinderTitleRow, findsOneWidget);
+      });
+      testWidgets('[GameEntryNameListRow Widget] has a GameEntryNameListRowPlayerNameList.', (
+        WidgetTester tester,
+      ) async {
+        await tester.pumpWidget(wrappedWidget);
+        final widgetFinderTitleRow = find.byType(GameEntryNameListRowPlayerNameList);
+        expect(widgetFinderTitleRow, findsOneWidget);
+      });
+    });
+  });
+}
