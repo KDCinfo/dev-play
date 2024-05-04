1290346554183cf8bea708299eaf114503a0d4f8
Keith D Commiskey <atlassian@kdcinfo.com>
2024-05-04 06:34:36 -0700

    Added fake `GamePlayer` data for testing.

---
+++ tictactoe/lib/src/app_data.dart
@@ -0,0 +1,32 @@
+import 'package:dev_play_tictactoe/src/screens/game_widgets/game_widgets.dart';
+
+abstract class AppData {
+  /// Only fully populated players can be in the list,
+  ///   because they've played before.
+  ///
+  /// Reminder: `playerNum` and `userSymbol` are transient and do
+  ///   not need to be unique for storing, only when playing.
+  static final fakePlayerList3 = <GamePlayer>[
+    const GamePlayer(
+      playerId: 100, //                 Unique
+      playerName: 'John', //            Unique
+      playerNum: 1, //                  Not unique
+      userSymbol: UserSymbolX(), //     Not unique
+      playerType: PlayerTypeHuman(), // Not unique
+    ),
+    const GamePlayer(
+      playerId: 101,
+      playerName: 'Jane',
+      playerNum: 2,
+      userSymbol: UserSymbolO(),
+      playerType: PlayerTypeHuman(),
+    ),
+    const GamePlayer(
+      playerId: 102,
+      playerName: 'Jim',
+      playerNum: 1,
+      userSymbol: UserSymbolX(),
+      playerType: PlayerTypeHuman(),
+    ),
+  ];
+}
