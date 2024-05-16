52832907ecea95ad16e37bc6d7e193e0b9693d3c
Keith D Commiskey <atlassian@kdcinfo.com>
2024-05-16 02:58:10 -0700

    Added Scorebook `RepositoryProvider` w StorageApi.
    The `StorageAPI` includes methods for both `Shared Preferences` and `Flutter Secure Storage`.

--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -1,9 +1,14 @@
 import 'dart:developer';
 
+import 'package:base_services/base_services.dart';
+
 import 'package:dev_play_tictactoe/src/app.dart';
+import 'package:dev_play_tictactoe/src/data/data.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+Future<void> main() async {
   /// Logging will be enabled when not in production.
   const isProd = false;
 
@@ -18,6 +23,27 @@ import 'package:flutter/material.dart';
     log('App initialized: ${now.hour}:${now.minute}:${now.second}.${now.millisecond}');
   }
 
-void main() {
-  runApp(const MyApp());
+  /// APIs
+  ///
+  final sharedPrefsApi = await SharedPrefsApi.init();
+  final secureStorageApi = FlutterSecureStorageApi();
+
+  /// Services and Repositories
+  ///
+  final storageApi = StorageServiceImpl(
+    localStorageApi: sharedPrefsApi,
+    localSecureStorageApi: secureStorageApi,
+    canPrint: !isProd,
+  );
+
+  final scorebookRepository = ScorebookRepository(
+    storageService: storageApi,
+  );
+
+  runApp(
+    RepositoryProvider.value(
+      value: scorebookRepository,
+      child: const MyApp(),
+    ),
+  );
 }
