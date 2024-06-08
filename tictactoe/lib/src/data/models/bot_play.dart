772b5068492b14c8a0a430e22f88516b75c0bc25
Keith D Commiskey <github@kdcinfo.com>
2024-06-07 19:46:30 -0700

    Updated draft of `BotPlay` with tests (WIP).

--- tictactoe/lib/src/data/models/bot_play.dart
+++ tictactoe/lib/src/data/models/bot_play.dart
@@ -47,7 +47,7 @@ abstract class BotPlay {
     };
 
     /// The `edgeSize` is the number of rows or columns.
-    final edgeSize = countDownRows.values.first.length;
+    // final edgeSize = countDownRows.values.first.length;
 
     // countDownRows.entries.map((rowGroup) {
     countDownRows.forEach(
@@ -56,137 +56,61 @@ abstract class BotPlay {
         //
         // rowGroup.key // 0 // `groupIndex`
         // rowGroup.value // [0, 1, 1, -2, 1]
-        //
-        /// This will allow for this tile, and future marker tiles,
-        /// and now player tiles, to all look to update the `tupleDataLists`.
-        var markerHitStartCompares = false;
 
-        /// <playerId, currentLongestCount>
-        // final prevIdCount = <int, int>{};
-        /// previousIdTracker | <playerId, currentLongestCount, loopIndex>
-        final previousIdTracker = <int, int>{};
-        var loopCount = 0;
+        // const previousIdTracker = PreviousIdTracker(
+        //     // playerId: -1,
+        //     // currentLongestCount: 0,
+        //     // loopIndex: -1,
+        //     );
+        // var loopCount = 0;
 
-        /// Check for empty tiles in each row.
+        /// Only check rows with an empty tile.
         final rowHasEmptyTile = groupList.any((playerId) => playerId == -2);
         final rowHasAllEmptyTiles = groupList.every((playerId) => playerId == -2);
 
         /// If there are no empty tiles, there's nowhere to play and nothing to check for.
         if (!rowHasAllEmptyTiles && rowHasEmptyTile) {
-          // for (final checkPlayerId in rowGroup.value) {
-          for (final checkPlayerId in groupList) {
-            // key:   int       currentTileIndex
-            // value: List<int> checkPlayerId
+          // for (final checkPlayerId in groupList) {
+          // key:   int       currentTileIndex
+          // value: List<int> checkPlayerId
 
-            /// When the first marker is hit; we can then begin
-            /// performing checks on whether to update `tupleDataLists`.
-            if (checkPlayerId == -2 && !markerHitStartCompares) {
-              markerHitStartCompares = true;
-            }
+          var currentRangeLength = 0;
+          var maxRangeLength = 0;
+          var lastEmptyTileIndex = -1;
+          var bestTileIndex = -1;
 
-            if (previousIdTracker.isEmpty) {
-              // There's no need to store empty tiles.
-              if (checkPlayerId != -2) {
-                previousIdTracker.addAll({checkPlayerId: 1});
-
-                tupleDataLists[MatchTupleEnum.row] = tupleDataLists[MatchTupleEnum.row]!.copyWith(
-                  // matchTupleEnum: MatchTupleEnum.row,
-                  playerId: previousIdTracker.keys.first,
-                  tilesPlayedCount: previousIdTracker.values.first,
-                  groupIndex: rowGroup,
-                  // tileIndexToPlay: -1,
-                );
-              } else {
-                tupleDataLists[MatchTupleEnum.row] = tupleDataLists[MatchTupleEnum.row]!.copyWith(
-                  // matchTupleEnum: MatchTupleEnum.row,
-                  // playerId: prevIdCount.keys.first,
-                  // tilesPlayedCount: prevIdCount.values.first,
-                  groupIndex: rowGroup,
-                  tileIndexToPlay: loopCount + (rowGroup * edgeSize),
-                );
+          // Scan through the current row of tiles.
+          for (var loopCount = 0; loopCount < groupList.length; loopCount++) {
+            if (groupList[loopCount] == -2) {
+              // Check if the current range length is greater than the max range length found so far.
+              if (currentRangeLength > maxRangeLength) {
+                maxRangeLength = currentRangeLength;
+                bestTileIndex = lastEmptyTileIndex == -1 ? loopCount : lastEmptyTileIndex;
               }
+              // Update the last empty tile index.
+              lastEmptyTileIndex = loopCount;
+              // Reset the current range length.
+              currentRangeLength = 0;
             } else {
-              ///
-              /// [ 1 ] --- Pre-check; update `tupleDataLists`
-              ///
-              /// This first code block is for cases where a marker isn't the
-              /// first cell, and it has to look at the sequential tiles 'before'
-              /// the current tile, which is the `prevIdCount` before the update below.
-              ///
-              /// So before updating the `prevIdCount`, if this is an empty
-              /// tile (`-2`), we need to check the previous `prevIdCount`
-              /// to see if it qualifies for updating `tupleDataLists`.
-              ///
-              /// @TODO: Need to know which tile index last had a play.
-              ///
-              if (checkPlayerId == -2 && tupleDataLists[MatchTupleEnum.row]!.tileIndexToPlay == -1
-                  // && (prevIdCount.values.first - loopCount).abs() <= 1
-                  ) {
-                if (previousIdTracker.values.first >=
-                    tupleDataLists[MatchTupleEnum.row]!.tilesPlayedCount) {
-                  //
-                  tupleDataLists[MatchTupleEnum.row] = tupleDataLists[MatchTupleEnum.row]!.copyWith(
-                    // matchTupleEnum: MatchTupleEnum.row,
-                    playerId: previousIdTracker.keys.first,
-                    tilesPlayedCount: previousIdTracker.values.first,
-                    groupIndex: rowGroup,
-                    tileIndexToPlay: loopCount + (rowGroup * edgeSize),
-                  );
-                }
-              }
-
-              /// [ 2 ] --- Update `prevIdCount`
-              ///
-              /// Update the running `prevIdCount`.
-              if (previousIdTracker.isNotEmpty && previousIdTracker.keys.contains(checkPlayerId)) {
-                final tempNewMax = previousIdTracker[checkPlayerId]! + 1;
-                previousIdTracker[checkPlayerId] = tempNewMax;
-              } else {
-                // There's no need to store empty tiles.
-                if (checkPlayerId != -2) {
-                  previousIdTracker
-                    ..clear()
-                    ..addAll({checkPlayerId: 1});
-                }
-              }
-
-              /// [ 3 ] --- Post-check; update `tupleDataLists`
-              ///
-              /// Now that `prevIdCount` has been updated with
-              /// this current tile, we need to check if the new
-              /// data qualifies it for updating `tupleDataLists`.
-              if (markerHitStartCompares && checkPlayerId != -2) {
-                // final prevTileIdEmpty = tupleDataLists[MatchTupleEnum.row]!.playerId == -2;
-                // if (prevTileIdEmpty || prevIdCount.values.first >=
-                if (previousIdTracker.values.first >=
-                    tupleDataLists[MatchTupleEnum.row]!.tilesPlayedCount) {
-                  //
-                  tupleDataLists[MatchTupleEnum.row] = tupleDataLists[MatchTupleEnum.row]!.copyWith(
-                    // matchTupleEnum: MatchTupleEnum.row,
-                    playerId: previousIdTracker.keys.first,
-                    tilesPlayedCount: previousIdTracker.values.first,
-                    groupIndex: rowGroup,
-                    // tileIndexToPlay: loopCount,
-                  );
-                }
-              } else if (checkPlayerId == -2 &&
-                  (previousIdTracker.values.first - loopCount).abs() <= 1) {
-                if (previousIdTracker.values.first >=
-                    tupleDataLists[MatchTupleEnum.row]!.tilesPlayedCount) {
-                  //
-                  tupleDataLists[MatchTupleEnum.row] = tupleDataLists[MatchTupleEnum.row]!.copyWith(
-                    // matchTupleEnum: MatchTupleEnum.row,
-                    // playerId: prevIdCount.keys.first,
-                    // tilesPlayedCount: prevIdCount.values.first,
-                    groupIndex: rowGroup,
-                    // tileIndexToPlay: loopCount,
-                    tileIndexToPlay: loopCount + (rowGroup * edgeSize),
-                  );
-                }
-              }
+              // Increment the current range length.
+              currentRangeLength++;
             }
+          }
 
-            loopCount++;
+          // Final check at the end of the array.
+          if (currentRangeLength > maxRangeLength) {
+            maxRangeLength = currentRangeLength;
+            // `-1 ? loopCount + 1` is for when the first tiles (`0+`) are played (not empty).
+            bestTileIndex = lastEmptyTileIndex;
+          }
+
+          if (maxRangeLength >= tupleDataLists[MatchTupleEnum.row]!.tilesPlayedCount) {
+            // return bestTileIndex;
+            tupleDataLists[MatchTupleEnum.row] = tupleDataLists[MatchTupleEnum.row]!.copyWith(
+              tilesPlayedCount: maxRangeLength,
+              groupIndex: rowGroup,
+              tileIndexToPlay: bestTileIndex + (rowGroup * groupList.length),
+            );
           }
         }
         // return rowGroup;
@@ -203,5 +127,59 @@ abstract class BotPlay {
     // return newScorebookData.updateGame(newGameData);
     final tileIndexToPlay = tupleDataLists[MatchTupleEnum.row]!.tileIndexToPlay;
     return tileIndexToPlay > -1 ? tileIndexToPlay : 0;
+  }
+
+  static int findBestTileIndex(
+    List<int> tiles,
+    int nonBotPlayerId,
+  ) {
+    // Capture sublists of non-bot played tiles.
+    final sublists = <List<int>>[];
+    final currentSublist = <int>[];
+
+    for (var i = 0; i < tiles.length; i++) {
+      if (tiles[i] == nonBotPlayerId) {
+        // Store index of player 1 tiles.
+        currentSublist.add(i);
+      } else {
+        if (currentSublist.isNotEmpty) {
+          sublists.add(List.from(currentSublist));
+          currentSublist.clear();
+        }
+      }
+    }
+
+    if (currentSublist.isNotEmpty) {
+      sublists.add(List.from(currentSublist));
+    }
+
+    // Find the largest sublist.
+    final largestSublist = sublists.fold(<int>[], (a, b) => a.length > b.length ? a : b);
+
+    // Check for empty tiles adjacent to the largest sublist.
+    final leftIndex = largestSublist.isEmpty ? -1 : largestSublist.first - 1;
+    final rightIndex = largestSublist.isEmpty ? -1 : largestSublist.last + 1;
+
+    // We need to check both sides and choose the first valid one.
+    if (leftIndex >= 0 && tiles[leftIndex] == -2) {
+      return leftIndex;
+    }
+    if (rightIndex < tiles.length && rightIndex >= 0 && tiles[rightIndex] == -2) {
+      return rightIndex;
+    }
+
+    // If no empty tiles are found next to the largest sublist,
+    // check the first and last elements.
+    for (var i = 0; i < tiles.length; i++) {
+      if (tiles[i] == -2) {
+        if ((i > 0 && tiles[i - 1] == nonBotPlayerId) ||
+            (i < tiles.length - 1 && tiles[i + 1] == nonBotPlayerId)) {
+          return i;
+        }
+      }
+    }
+
+    // If no adjacent empty tiles are found, return -1 (this should not happen given the problem constraints).
+    return -1;
   }
 }
--- tictactoe/test/data/models/bot_play_test.dart
+++ tictactoe/test/data/models/bot_play_test.dart
@@ -5,40 +5,158 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('[BotPlay] Testing:', () {
-    test('runBotPlay should return 0 when there are no empty tiles.', () {
-      final filledAllRows = {
-        MatchTupleEnum.row: {
-          0: [0, 1, 1, 0, 1],
-          1: [-2, -2, 1, 0, -2],
-          2: [0, 1, -2, 0, 1],
-          3: [0, 1, 1, -2, 1],
-          4: [0, 1, 1, 0, 1],
-        },
-      };
+    group('[runBotPlay]', () {
+      test('runBotPlay should return 0 when there are no empty tiles.', () {
+        final filledAllRows = {
+          MatchTupleEnum.row: {
+            0: [0, 1, 1, 0, 1],
+            1: [-2, -2, 1, 0, -2],
+            2: [0, 1, -2, 0, 1],
+            3: [0, 1, 1, -2, 1],
+            4: [0, 1, 1, 0, 1],
+          },
+        };
 
-      final result = BotPlay.runBotPlay(filledAllRows: filledAllRows);
+        final result = BotPlay.runBotPlay(filledAllRows: filledAllRows);
 
-      // @TODO: Finish this test when `BotPlay.runBotPlay` is completed.
-      // expect(result, 0);
-      expect(result, 18);
+        // @TODO: Finish this test when `BotPlay.runBotPlay` is completed.
+        // expect(result, 0);
+        expect(result, 18);
+      });
+
+      test('runBotPlay should return the tile index to play when there are empty tiles.', () {
+        final filledAllRows = {
+          MatchTupleEnum.row: {
+            0: [0, 1, 1, -2, 1],
+            1: [-2, -2, 1, 0, -2],
+            2: [0, 1, -2, 0, 1],
+            3: [0, 1, 1, -2, 1],
+            4: [0, 1, 1, 0, 1],
+          },
+        };
+
+        final result = BotPlay.runBotPlay(filledAllRows: filledAllRows);
+
+        // @TODO: Finish this test when `BotPlay.runBotPlay` is completed.
+        // expect(result, 3);
+        expect(result, 18);
+      });
     });
 
-    test('runBotPlay should return the tile index to play when there are empty tiles.', () {
-      final filledAllRows = {
-        MatchTupleEnum.row: {
-          0: [0, 1, 1, -2, 1],
-          1: [-2, -2, 1, 0, -2],
-          2: [0, 1, -2, 0, 1],
-          3: [0, 1, 1, -2, 1],
-          4: [0, 1, 1, 0, 1],
-        },
-      };
+    group('[findBestTileIndex]', () {
+      const emptyId = -2;
+      const playId = 5;
+      const botId = 2;
 
-      final result = BotPlay.runBotPlay(filledAllRows: filledAllRows);
+      const checkId = 5;
+      test('starting with playerId -2 (empty) with player 1:', () {
+        expect(
+          BotPlay.findBestTileIndex([emptyId, emptyId, emptyId, emptyId, emptyId], checkId),
+          -1,
+        );
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, emptyId, emptyId, playId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, emptyId, playId, playId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, emptyId, playId, botId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, emptyId, botId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, playId, emptyId, emptyId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, playId, playId, playId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, playId, botId, playId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, botId, playId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, emptyId, emptyId, emptyId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, emptyId, emptyId, playId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, emptyId, playId, emptyId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, emptyId, playId, playId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, playId, emptyId, emptyId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, playId, emptyId, botId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, playId, playId, emptyId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, playId, playId, playId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, playId, botId, botId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, emptyId, playId, playId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, playId, playId, emptyId], checkId), 4);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, playId, playId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, botId, playId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, botId, botId, botId], checkId), -1);
+      });
 
-      // @TODO: Finish this test when `BotPlay.runBotPlay` is completed.
-      // expect(result, 3);
-      expect(result, 18);
+      test('starting with playerId -2 (empty) mixed with players 1 and 2:', () {
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, playId, playId, botId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, playId, botId, botId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, botId, playId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, botId, botId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([emptyId, emptyId, botId, botId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, emptyId, playId, botId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, emptyId, botId, playId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, emptyId, botId, botId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, playId, botId, emptyId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, botId, emptyId, playId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, botId, emptyId, botId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, botId, playId, emptyId], checkId), 4);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, botId, playId, botId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, botId, botId, emptyId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, botId, botId, playId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, playId, botId, botId, botId], checkId), 0);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, emptyId, playId, botId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, emptyId, botId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, emptyId, botId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, playId, emptyId, playId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([emptyId, botId, playId, emptyId, botId], checkId), 3);
+      });
+
+      test('starting with player 1:', () {
+        expect(BotPlay.findBestTileIndex([playId, emptyId, emptyId, emptyId, emptyId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([playId, emptyId, emptyId, emptyId, botId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([playId, emptyId, emptyId, botId, botId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([playId, emptyId, botId, botId, botId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([playId, playId, emptyId, emptyId, emptyId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([playId, playId, emptyId, botId, botId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([playId, playId, playId, emptyId, emptyId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([playId, playId, playId, playId, emptyId], checkId), 4);
+        expect(BotPlay.findBestTileIndex([playId, playId, playId, botId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([playId, playId, botId, playId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([playId, playId, botId, botId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([playId, botId, playId, playId, emptyId], checkId), 4);
+        expect(BotPlay.findBestTileIndex([playId, botId, playId, playId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([playId, botId, playId, botId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([playId, botId, botId, playId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([playId, botId, botId, playId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([playId, botId, botId, botId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([playId, botId, botId, botId, botId], checkId), -1);
+      });
+
+      test('starting with player 2:', () {
+        expect(BotPlay.findBestTileIndex([botId, emptyId, emptyId, emptyId, emptyId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([botId, emptyId, emptyId, playId, playId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([botId, emptyId, playId, playId, playId], checkId), 1);
+        expect(BotPlay.findBestTileIndex([botId, playId, emptyId, emptyId, emptyId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([botId, playId, emptyId, emptyId, playId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([botId, playId, emptyId, playId, emptyId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([botId, playId, emptyId, playId, playId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([botId, playId, playId, emptyId, emptyId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([botId, playId, playId, emptyId, botId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([botId, playId, playId, playId, emptyId], checkId), 4);
+        expect(BotPlay.findBestTileIndex([botId, playId, playId, playId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([botId, playId, playId, playId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([botId, playId, playId, botId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([botId, botId, emptyId, emptyId, playId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([botId, botId, emptyId, playId, playId], checkId), 2);
+        expect(BotPlay.findBestTileIndex([botId, botId, playId, playId, emptyId], checkId), 4);
+        expect(BotPlay.findBestTileIndex([botId, botId, playId, playId, botId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([botId, botId, botId, playId, playId], checkId), -1);
+      });
+
+      test('additional edge cases and patterns:', () {
+        // Single player dominance.
+        expect(BotPlay.findBestTileIndex([playId, playId, playId, playId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([botId, botId, botId, botId, botId], checkId), -1);
+
+        // Alternating patterns.
+        expect(BotPlay.findBestTileIndex([playId, botId, playId, botId, playId], checkId), -1);
+        expect(BotPlay.findBestTileIndex([botId, playId, botId, playId, botId], checkId), -1);
+
+        // Interspersed empty tiles.
+        expect(BotPlay.findBestTileIndex([playId, emptyId, playId, emptyId, playId], checkId), 3);
+        expect(BotPlay.findBestTileIndex([botId, emptyId, botId, emptyId, botId], checkId), -1);
+      });
     });
   });
 }
