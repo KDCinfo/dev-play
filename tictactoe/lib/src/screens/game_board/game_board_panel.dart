d8449417edc6e77792cdb758cbb5bcf19921f2df
Keith D Commiskey <github@kdcinfo.com>
2024-06-11 05:25:26 -0700

    Added `BlocBuilder` with `LinearProgressIndicator`

--- tictactoe/lib/src/screens/game_board/game_board_panel.dart
+++ tictactoe/lib/src/screens/game_board/game_board_panel.dart
@@ -30,48 +30,73 @@ class GameBoardPanel extends StatelessWidget {
 
         return LayoutBuilder(
           builder: (context, constraints) {
-            return Align(
-              // alignment: Alignment.center,
-              // Although `.center` is the default, the `Align` wrapper is still required
-              // in conjunction with the `AspectRatio` widget wrapper to contain the grid
-              // within the available viewing area, else it will expand and get cropped.
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: GridView.builder(
-                  itemCount: tileCount,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: edgeSize, // Number of columns.
-                    // childAspectRatio: 1, // Aspect ratio of each tile.
-                  ),
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: clickableTile(index) ? () => gridTileCallback(index, context) : null,
-                      child: GridTile(
-                        child: Stack(
-                          children: [
-                            GameBoardPanelTile(index),
-                            // @TODO: Maybe try to make the icon pop up when tapped.
-                            Positioned(
-                              // Max width and height of the clickable area.
-                              // height: double.infinity,
-                              // width: double.infinity,
-                              left: 0,
-                              right: 0,
-                              top: 0,
-                              bottom: 0,
-                              child: GameBoardPanelTileOverlay(
-                                index: index,
-                                currentGame: state.currentGame,
-                                isClickableTile: clickableTile(index),
+            return BlocBuilder<WaitForBotBloc, WaitForBotState>(
+              builder: (context, waitState) {
+                return Align(
+                  // alignment: Alignment.center,
+                  // Although `.center` is the default, the `Align` wrapper is still required
+                  // in conjunction with the `AspectRatio` widget wrapper to contain the grid
+                  // within the available viewing area, else it will expand and get cropped.
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: Stack(
+                      children: [
+                        GridView.builder(
+                          itemCount: tileCount,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: edgeSize, // Number of columns.
+                            // childAspectRatio: 1, // Aspect ratio of each tile.
+                          ),
+                          itemBuilder: (context, index) {
+                            return InkWell(
+                              onTap: !waitState.isWaiting && clickableTile(index)
+                                  ? () => gridTileCallback(index, context)
+                                  : null,
+                              child: GridTile(
+                                child: Stack(
+                                  children: [
+                                    GameBoardPanelTile(index),
+                                    // @TODO: Maybe try to make the icon pop up when tapped.
+                                    Positioned(
+                                      // Max width and height of the clickable area.
+                                      // height: double.infinity,
+                                      // width: double.infinity,
+                                      left: 0,
+                                      right: 0,
+                                      top: 0,
+                                      bottom: 0,
+                                      child: GameBoardPanelTileOverlay(
+                                        index: index,
+                                        currentGame: state.currentGame,
+                                        isClickableTile: clickableTile(index),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        // const ColoredBox(color: Colors.green),
+                        if (waitState.isWaiting)
+                          Center(
+                            child: SizedBox(
+                              height: 100,
+                              width: 120,
+                              child: LinearProgressIndicator(
+                                backgroundColor: AppConstants.primaryTileColor.withOpacity(0.7),
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                  Colors.deepOrange.withOpacity(0.4),
+                                ),
+                                borderRadius: BorderRadius.circular(50),
                               ),
                             ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+                          ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             );
           },
         );
