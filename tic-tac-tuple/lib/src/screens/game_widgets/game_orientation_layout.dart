97367c8a42b66d9ff38858838575791e966b900c
Keith D Commiskey <github@kdcinfo.com>
2024-06-19 04:01:23 -0700

    Fixed issue with keyboard closing on small devices
    Well, half fixed; portrait is good but landscape is now also portrait as a result. Added `@TODO:`.

--- tic-tac-tuple/lib/src/screens/game_widgets/game_orientation_layout.dart
+++ tic-tac-tuple/lib/src/screens/game_widgets/game_orientation_layout.dart
@@ -41,16 +41,56 @@ class GameOrientationLayout extends StatelessWidget {
           ),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 30),
-            child: LayoutBuilder(
-              builder: (context, checkConstraints) {
-                /// Determine available height minus static elements.
-                final availableHeight = checkConstraints.maxHeight - 60 - 40 - 10;
+            child: OrientationBuilder(
+              builder: (context, orientation) {
+                // The 'orientation checker' commented out below has the same
+                // issue as the constraint logic in the solution below, which
+                // is that when the keyboard appears, it changes the layout dimensions,
+                // confusing smaller screen constraints into thinking the screen
+                // is in landscape mode and swapping out the layouts.
+                //
+                // return orientation == Orientation.landscape
+                //     ? orientationScreen.landscape
+                //     : orientationScreen.portrait;
+                //
+                return LayoutBuilder(
+                  builder: (context, checkConstraints) {
+                    //
+                    // Hack Solution: Do not calculate screen layout if the keyboard is up.
+                    //                Note that this cripples landscape mode (noted below).
+                    //
+                    if (MediaQuery.of(context).viewInsets.bottom > 0) {
+                      // Determine available height minus static elements.
+                      final availableHeight = checkConstraints.maxHeight - 60 - 40 - 10;
 
-                return availableHeight < 300 // + 110 == 410
-                    /// For landscape or smaller screens, the GameEntry screen will have its
-                    /// board size and buttons to the right of the player list.
-                    ? orientationScreen.landscape
-                    : orientationScreen.portrait;
+                      return availableHeight < 300 // + 110 == 410
+                          // For landscape or vertically-challenged devices, the
+                          // `GameEntry` screen will have its board size and
+                          // buttons placed to the right of the player list.
+                          ? orientationScreen.landscape
+                          : orientationScreen.portrait;
+                    } else {
+                      //
+                      // @TODO: Find a way to detect the actual orientation of the device
+                      //        (i.e. not based on the layout constraints).
+                      //
+                      // If in landscape mode, when the keyboard comes up,
+                      //   the return below will cause the same issue as above.
+                      //
+                      // So in effect, landscape can only be used when playing
+                      //   with the default names (i.e. cannot use `TextFormField`s).
+                      //
+                      return orientationScreen.portrait;
+                      //
+                      // Maybe we can store the portrait/landscape mode on initial load,
+                      //   then only perform the logic above when the screen actually rotates.
+                      //
+                      // The fallacy of that is the orientation detection also
+                      //   detects based on the view constraints (i.e. when the keyboard
+                      //   comes up, it will still think the device flipped orientation).
+                    }
+                  },
+                );
               },
             ),
           ),
