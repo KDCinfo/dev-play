d7ebe7cd5671e2fe68dead042108e7ba048e0bc6
Keith D Commiskey <github@kdcinfo.com>
2024-06-21 00:05:55 -0700

    Added `BoardSize` listener for GameEntry reset.

--- tic-tac-tuple/lib/src/screens/game_entry/game_entry_board_size_row.dart
+++ tic-tac-tuple/lib/src/screens/game_entry/game_entry_board_size_row.dart
@@ -47,51 +47,59 @@ class _GameEntryBoardSizeRowState extends State<GameEntryBoardSizeRow> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            boardSizeLabel,
-            key: boardSizeLabelKey,
-            style: AppConstants.headlineSmallTextStyle,
-            softWrap: false,
-            overflow: TextOverflow.ellipsis,
-          ),
-          const SizedBox(height: 4),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.4,
-            height: 1,
-            color: AppConstants.primaryTileColor.withOpacity(0.1),
-          ),
-          const SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                for (var labelIndex = 0; labelIndex < boardSizes.length; labelIndex++)
-                  Text(
-                    boardSizes.elementAtOrNull(labelIndex) ?? '3x3',
-                    key: Key(AppConstants.sliderLabelKey(labelIndex)),
-                  ),
-              ],
+    return BlocListener<GameEntryBloc, GameEntryState>(
+      listenWhen: (previous, current) => previous.edgeSize != current.edgeSize,
+      listener: (context, state) {
+        setState(() {
+          currentBoardSize = state.edgeSize.toDouble() - boardSizesOffset;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              boardSizeLabel,
+              key: boardSizeLabelKey,
+              style: AppConstants.headlineSmallTextStyle,
+              softWrap: false,
+              overflow: TextOverflow.ellipsis,
             ),
-          ),
-          BlocBuilder<GameEntryBloc, GameEntryState>(
-            builder: (context, state) {
-              return Slider(
-                key: boardSizeSliderKey,
-                value: currentBoardSize,
-                max: boardSizes.length.toDouble() - 1,
-                onChanged: inputChanged,
-                onChangeEnd: inputChangeCompleted,
-                divisions: boardSizes.length - 1,
-              );
-            },
-          ),
-        ],
+            const SizedBox(height: 4),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.4,
+              height: 1,
+              color: AppConstants.primaryTileColor.withOpacity(0.1),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  for (var labelIndex = 0; labelIndex < boardSizes.length; labelIndex++)
+                    Text(
+                      boardSizes.elementAtOrNull(labelIndex) ?? '3x3',
+                      key: Key(AppConstants.sliderLabelKey(labelIndex)),
+                    ),
+                ],
+              ),
+            ),
+            BlocBuilder<GameEntryBloc, GameEntryState>(
+              builder: (context, state) {
+                return Slider(
+                  key: boardSizeSliderKey,
+                  value: currentBoardSize,
+                  max: boardSizes.length.toDouble() - 1,
+                  onChanged: inputChanged,
+                  onChangeEnd: inputChangeCompleted,
+                  divisions: boardSizes.length - 1,
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
