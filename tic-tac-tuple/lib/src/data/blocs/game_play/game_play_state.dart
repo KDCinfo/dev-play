1dccf212303717447017297687b8d9eaccdd61c8
Keith D Commiskey <github@kdcinfo.com>
2024-06-21 00:04:36 -0700

    Added `pausedGame` property to GamePlay bloc state

--- tic-tac-tuple/lib/src/data/blocs/game_play/game_play_state.dart
+++ tic-tac-tuple/lib/src/data/blocs/game_play/game_play_state.dart
@@ -3,21 +3,25 @@ part of 'game_play_bloc.dart';
 class GamePlayState extends Equatable {
   const GamePlayState({
     this.currentGame = const GameData(),
+    this.pausedGame = const GameData(),
   });
 
   final GameData currentGame;
+  final GameData pausedGame;
 
-  /// copyWith
   GamePlayState copyWith({
     GameData? currentGame,
+    GameData? pausedGame,
   }) {
     return GamePlayState(
       currentGame: currentGame ?? this.currentGame,
+      pausedGame: pausedGame ?? this.pausedGame,
     );
   }
 
   @override
   List<Object> get props => [
         currentGame,
+        pausedGame,
       ];
 }
